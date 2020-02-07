60ab5b2db8f214ea5c56cce02a48151c49d66705
nesvila90 <nesvila90@hotmail.com>
2020-02-07 01:15:11 -0500

    Add files via upload

---
+++ hexagonal-architecture-poc/hexagonal/bootloader/src/main/java/com/indra/base/bootloader/BootloaderApplication.java
@@ -0,0 +1,13 @@
+package com.indra.base.bootloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootloaderApplication.class, args);
+	}
+
+}
